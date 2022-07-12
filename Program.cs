826f2e24fb0f94d83f0681e8cc52a96bd9d4ec81
Luis Antónimo <luisantonimo@gmail.com>
2022-07-12 09:14:21 +0200

    ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace T04Ejercicio23
+{
+    class CalculadoraInversa
+    {
+        static void Main()
+        {
+            Console.WriteLine("Escriba el primer operando (A): ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escriba el segundo operando (B): ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escriba el signo aritmético (+,-,*,/,^,%): ");
+            String signo = Console.ReadLine();
+
+            switch (signo)
+            {
+                case "+":
+                    Console.WriteLine("El resultado de A+B es: " + (a + b));
+                    break;
+                case "-":
+                    Console.WriteLine("El resultado de A-B es: " + (a - b));
+                    break;
+                case "*":
+                    Console.WriteLine("El resultado de A*B es: " + (a * b));
+                    break;
+                case "/":
+                    double aDoble = Convert.ToDouble(a);
+                    double bDoble = Convert.ToDouble(b);
+                    Console.WriteLine("El resultado de A/B es: " + (aDoble / bDoble));
+                    break;
+                case "^":
+                    Console.WriteLine("El resultado de A^B es: " + Math.Pow(a, b));
+                    break;
+                case "%":
+                    Console.WriteLine("El resultado de A%B es: " + (a % b));
+                    break;
+                default:
+                    Console.WriteLine("No ha escrito la operación correctamente.");
+                    break;
+            }
+        }
+    }
+}
